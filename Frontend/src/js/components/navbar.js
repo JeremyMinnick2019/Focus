c1ac07896bfb7d8b30d21be28a2f119da96337c5
Bryan Kautzman <bdkautz@icloud.com>
2019-11-19 15:10:19 -0500

    navbar commit2

--- Frontend/src/js/components/navbar.js
+++ Frontend/src/js/components/navbar.js
@@ -3,20 +3,20 @@ export default function Navbar() {
     
     
     <nav class='navbar'>
-    <div class='navwrap'>
         <ul>
             <li><a id="nav__home">Home</a></li>
             <li><a id="nav__profile">Profile</a></li>
             <li><a id="nav__todo">To-Do</a></li>
             <li><a id="nav__planner">Planner</a></li>
         </ul>
-        <div class="burger">
+        <div class="burger" onclick="myFunction(this)">
             <div class="line1"></div>
             <div class="line2"></div>
             <div class="line3"></div>
         </div>
+
         
-    </div>
     </nav>
     `;
 }
+
