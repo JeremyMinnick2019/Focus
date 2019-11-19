0a7f492e952bee55ef313521e5ac455da6803bab
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-11-19 11:13:27 -0500

    delete user function

--- Frontend/src/js/userFunction.js
+++ Frontend/src/js/userFunction.js
@@ -17,9 +17,35 @@ function displayUser(){
     const app = document.querySelector("#app");
     app.addEventListener("click", function(){
         if(event.target.classList.contains("userName")){
-            const userid = event.target.paerntElement.querySelector(".user_id").value;
+            const userid = event.target.parentElement.querySelector(".user_id").value;
             apiActions.getRequest(`https://localhost:44306/api/users/${userid}`, users =>{
                 document.querySelector("#app").innerHTML = User(users);
+            })
+        }
+    })
+
+    app.addEventListener("click", function(){
+        const addUser = event.target.parentElement.querySelector("add-user_name").value;
+        const addEmail = event.target.parentElement.querySelector("add-user_email").value;
+        const addPhone = event.target.parentElement.querySelector("add-user_phone").value;
+
+        apiActions.postRequest("https://localhost:44306/api/users",
+        {
+            name: addUser,
+            email: addEmail,
+            phone: addPhone
+        },
+        users => {
+            document.querySelector("#app").innerHTML = User(users)
+        })
+    })
+
+    app.addEventListener("click", function(){
+        if(event.target.classList.contains("delete-user")){
+            const userid = event.target.parentElement.querySelector(".user_id").value;
+            apiActions.deleteRequest(`https://localhost:44306/api/users/${userid}`,
+            users => {
+                app.innerHTML = User(users);
             })
         }
     })
