5612813860d4b879e23bac1e5ccc97a3e1f20dd8
Jeremy <JeremyMinnick2019@Gmail.com>
2019-11-19 11:10:37 -0500

    Add business

--- Frontend/src/js/businessFunction.js
+++ Frontend/src/js/businessFunction.js
@@ -13,4 +13,33 @@ function displayBusiness(){
             document.querySelector("#app").innerHTML = Business(businesses);
         })
     })
+
+    const app = document.querySelector("#app");
+    app.addEventListener("click", function(){
+        if(event.target.classList.contains("businessTitle")){
+        const businessId = event.target.parentElement.querySelector(".business_id").value;
+        apiActions.getRequest(`https://localhost:44306/api/businesses/${businessId}`, businesses => {
+            document.querySelector("#app").innerHTML = Business(businesses);
+            console.log(businesses);
+        })
+
+    }
+})
+app.addEventListener("click", function(){
+    const addTitle = event.target.parentElement.querySelector(".add-business_title").value
+    const addIndustry = event.target.parentElement.querySelector(".add-business_industry").value
+    console.log(addTitle)
+    apiActions.postRequest
+    ( 
+        "https://localhost:44306/api/businesses",{  
+        title: addTitle,
+        industry: addIndustry
+        },
+
+        businesses => {
+            document.querySelector("#app").innerHTML = Businesses(businesses)
+        }
+       
+    )
+})
 }
--- Frontend/src/js/components/business.js
+++ Frontend/src/js/components/business.js
@@ -4,8 +4,7 @@ export default function Business(businesses) {
         .map(business => {
             return`
             <section class="business">
-                <h3>${business.title}</h3>
-                <h3>${business.zipcode}</h3>
+                <h3 class = "businessTitle">${business.title}</h3>
                 <h3>${business.industry}</h3>
                 <input class="business_id" type="hidden" value="${business.id}">
                 <button class="edit-business">Edit</button>
@@ -17,7 +16,6 @@ export default function Business(businesses) {
 
     <section class="businessAdd">
         <input class="add-business_title" type"text" placeholder="Add your business">
-        <input class="add-business_zipcode" type"text" placeholder="Add your zipcode">
         <input class="add-business_industry" type"text" placeholder="Add your business industry">
         <button class="add-business">Submit</button>
     </section>    
