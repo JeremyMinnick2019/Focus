286a693aa579ceba64cdcc9351da8487ba9f9227
Jeremy <JeremyMinnick2019@Gmail.com>
2019-11-19 12:15:45 -0500

    edit business

--- Frontend/src/js/businessFunction.js
+++ Frontend/src/js/businessFunction.js
@@ -42,4 +42,44 @@ app.addEventListener("click", function(){
        
     )
 })
+
+app.addEventListener("click", function(){
+    if(event.target.classList.contains("delete-business")) {
+        const businessId = event.target.parentElement.querySelector("business_id").value;
+        console.log("delete" + businessId);
+        apiActions.deleteRequest(`https://localhost:44306/api/businesses/${businessId}`,
+        businesses => {
+            app.innerHTML = Business(businesses)
+        })
+    }
+})
+
+app.addEventListener("click", function(){
+    if(event.target.classList.contains("edit-business")) {
+        const businessId = event.target.parentElement.querySelector("business_id").value;
+        console.log("edit"  + businessId);
+        apiActions.getRequest(`https://localhost:44306/api/businesses/${businessId}` , businesses => {
+            app.innerHTML = Business(businesses)
+        })
+    }
+})
+
+app.addEventListener("click", function() {
+    if(event,target.classList.contains(update-business)) {
+        const updateTitle = event.target.parentElement.querySelector(".update-business_title").value
+        const updateindustry = event.target.parentElement.querySelector(".update-business_industry").value
+
+        const businessData = {
+            title: updateTitle,
+            industry: updateindustry
+        }
+        apiActions.putRequest(
+            `https://localhost:44306/api/businesses/${businessId}`,
+            businessData,
+            businesses => {
+                document.querySelector("#app").innerHTML = Business(businesses)
+            }
+        )
+    }
+} )
 }
