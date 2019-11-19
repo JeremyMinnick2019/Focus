97d0a222b5593d3cb2d332b196fef58e4bbce244
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-11-19 11:40:11 -0500

    edit user crud

--- Frontend/src/js/userFunction.js
+++ Frontend/src/js/userFunction.js
@@ -12,7 +12,7 @@ function displayUser(){
         apiActions.getRequest("https://localhost:44306/api/users", users =>{
             document.querySelector("#app").innerHTML = User(users);
         })
-    })
+    });
 
     const app = document.querySelector("#app");
     app.addEventListener("click", function(){
@@ -38,7 +38,7 @@ function displayUser(){
         users => {
             document.querySelector("#app").innerHTML = User(users)
         })
-    })
+    });
 
     app.addEventListener("click", function(){
         if(event.target.classList.contains("delete-user")){
@@ -46,6 +46,36 @@ function displayUser(){
             apiActions.deleteRequest(`https://localhost:44306/api/users/${userid}`,
             users => {
                 app.innerHTML = User(users);
+            })
+        }
+    });
+
+    app.addEventListener("click", function(){
+        if(event.target.classList.contains("edit-user")){
+            const userid = event.target.parentElement.querySelector("user_id").value;
+            apiActions.getRequest(`https://localhost:44306/api/users/${userid}`,
+            userEdit => {
+                app.innerHTML = userEdit(editUser);
+            })
+        }
+    })
+
+    app.addEventListener("click", function(){
+        if(event.target.classList.contains("update-user_submit")){
+            const updateUser = event.target.parentElement.querySelector("update-user_name").value;
+            const updateEmail = event.target.parentElement.querySelector("update-user_email").value;
+            const updatePhone = event.target.parentElement.querySelector("update-user_phone").value;
+            
+            const userdata = 
+            {
+                name: updateUser,
+                email: updateEmail,
+                phone: updatePhone    
+            }
+            apiActions.putRequest(`https://localhost:44306/api/users/${userid}`,
+            userdata,
+            users => {
+                document.querySelector("#app").innerHTML = User(users);
             })
         }
     })
