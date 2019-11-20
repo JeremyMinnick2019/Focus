0ca4e18096d09dcf941defc9c3740e0346b72822
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-11-20 16:55:37 -0500

    style changes, grid

--- Frontend/src/js/components/business.js
+++ Frontend/src/js/components/business.js
@@ -1,11 +1,21 @@
 export default function Business(businesses) {
     return `
+        <section class="businessAdd">
+            <input class="add-business_name" type="text" placeholder="Add your business">
+            <input class="add-business_industry" type="text" placeholder="Add your business industry">
+            <select class= 'add-business_user' name="User">
+            <option value="1">John Smith</option>
+            <option value="2">Brad Jones</option>
+            </select>
+            <button class="add-business">Submit</button>
+        </section>
+
     ${businesses
         .map(business => {
             return `
             <section class="business">
                 <h3 class = "businessName">${business.name}</h3>
-                <h3>${business.industry}</h3>
+                <h5>${business.industry}</h5>
                 <input class="business_id" type="hidden" value="${business.id}">
                 <button class="edit-business">Edit</button>
                 <button class="delete-business">Delete</button>
@@ -14,14 +24,5 @@ export default function Business(businesses) {
         })
     .join("")}
 
-    <section class="businessAdd">
-        <input class="add-business_name" type="text" placeholder="Add your business">
-        <input class="add-business_industry" type="text" placeholder="Add your business industry">
-        <select class= 'add-business_user' name="User">
-        <option value="1">John Smith</option>
-        <option value="2">Brad Jones</option>
-        </select>
-        <button class="add-business">Submit</button>
-    </section>    
     `
 }
