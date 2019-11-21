70fb4ebfda4f9c5d3a61c65a3da1ecae01a0badc
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-11-21 12:50:49 -0500

    styling changes

--- Frontend/src/js/components/activity.js
+++ Frontend/src/js/components/activity.js
@@ -1,10 +1,11 @@
 export default function Activity(activities) {
     return `
-        <section class="activityAdd">
-            <input class="add-activity_name" type="text" placeholder="Add an Activity Name">
-            <input class="add-activity_importance" type="text" placeholder="Add an Activity Importance 1-10">
-            <input class="add-activity_urgency" type="text" placeholder="Add an Activity Urgency 1-10">
-            <select class="add-activity_categoryId" name="Category">
+    <section class="gridAct">
+        <section class="activityAdd" id="grid-Item">
+            <input class="add-activity_name" id="input" type="text" placeholder="Add an Activity Name">
+            <input class="add-activity_importance" id="input" type="text" placeholder="Add an Activity Importance 1-10">
+            <input class="add-activity_urgency" id="input" type="text" placeholder="Add an Activity Urgency 1-10">
+            <select class="add-activity_categoryId" id="input" name="Category">
                 <option value="1">Marketing</option>
                 <option value="2">Payroll</option>
                 <option value="3">Accounting/Auditing</option>
@@ -14,14 +15,14 @@ export default function Activity(activities) {
                 <option value="7">Operations</option>
                 <option value="8">Small Tasks</option>
             </select>
-            <textarea rows="10" cols="30" class="add-activity_description" placeholder="Add an Activity Description"></textarea>
-            <button class='add-activity'>Submit</button>
+            <textarea rows="5" cols="20" class="add-activity_description" id="input" placeholder="Add an Activity Description"></textarea>
+            <button class='add-activity' id="button">Submit</button>
         </section>
         
     ${activities
         .map(activity => {
             return `
-            <section class="activity">
+            <section class="activity" id="grid-Item">
                 <h3 class="activityName">${activity.name}</h3>
                 <h5>Importance: ${activity.importance}</h5>
                 <h5>Urgency: ${activity.urgency}</h5>
@@ -29,13 +30,13 @@ export default function Activity(activities) {
                 <h5>Created: ${activity.creation}</h5>
                 <h5>Completed: ${activity.completion}</h5>
                 <input class="activity_id" type="hidden" value="${activity.id}">
-                <button class="edit-activity">Edit</button>
-                <button class="delete-activity">Delete</button>
+                <button class="edit-activity" id="button">Edit</button>
+                <button class="delete-activity" id="button">Delete</button>
                 </section>
                 `;
             })
             .join("")}
-            
+    </section>
             `
 }
             // <select class="add-Activity_importance" name='importance' placeholder="Add Activity Importance">
--- Frontend/src/js/components/user.js
+++ Frontend/src/js/components/user.js
@@ -2,22 +2,22 @@ export default function User(users) {
     return `
     <section class="gridMain">
         <section class="userAdd" id="gridItem">
-            <input class="add-user_name" type="text" placeholder="Add your name">
-            <input class="add-user_email" type="text" placeholder="Add your email">
-            <input class="add-user_phone" type="text" placeholder="Add your phone number">
-            <button class='add-user'>Submit</button>
+            <input class="add-user_name" id="input" type="text" placeholder="Add your name">
+            <input class="add-user_email" id="input" type="text" placeholder="Add your email">
+            <input class="add-user_phone" id="input" type="text" placeholder="Add your phone number">
+            <button class='add-user' id="button">Submit</button>
         </section>
         
     ${users
         .map(user => {
             return `
-            <section class="user" id="gridItem">
+            <section class="user" id="gridItemA">
                 <h3 class="userName">${user.name}</h3>
                 <h5>${user.email}</h5>
                 <h5>${user.phone}</h5>
                 <input class="user_id" type="hidden" value="${user.id}">
-                <button class="edit-user">Edit</button>
-                <button class="delete-user">Delete</button>
+                <button class="edit-user" id="button">Edit</button>
+                <button class="delete-user" id="button">Delete</button>
             </section>
             `;
         })
