b4246ed8f875cbf6d210e71db588447b192b058e
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-11-27 12:40:47 -0500

    Belt level displays, switch works

--- Frontend/src/js/components/belt.js
+++ Frontend/src/js/components/belt.js
@@ -1,35 +1,42 @@
+import Points from "./points"
+import Activity from "./activity"
 export default () =>{
     Belt();
 }
 
 function Belt() {
-    const userBTN = document.querySelector("#profileButton");
+    // const userBTN = document.querySelector("#profileButton");
     const belts = document.querySelector("#belt");
-    userBTN.addEventListener("click", function(){
-        belts.innerHTML = `Get Your Belts here`;
-        let a = Activity.Rank;
-            switch (a)
-                        {
-                        case a >= 0:
-                            belts.innerHTML == `White Belt Level`;
-                        break;
-                        case a >= 51:
-                            belts.innerHTML == `Orange Belt Level`;
-                        break;
-            
-                        case a >= 101:
-                            belts.innerHTML == `Green Belt Level`;
-                        break;
-            
-                        case a >= 251:
-                            belts.innerHTML == `Brown Belt Level`;
-                        break;
-            
-                        case a >= 501:
-                            belts.innerHTML == `Black Belt Level`;
-                        break;
-                        }
-    })
+    // belts.innerHTML = `Get Your Belts here`;
+    // userBTN.addEventListener("click", function(){
+        const points = Activity.points;
+            switch (true){
+                //If points range 501-1000
+                case points >= 500:
+                    console.log("Black")
+                    belts.innerHTML = `Black Belt Level`;
+                    break;
+                //If points range 251-500
+                case points >= 250:
+                    console.log("Brown")
+                    belts.innerHTML = `Brown Belt Level`;
+                    break;
+                //If points range 101-250
+                case points >= 100:
+                    console.log("Green")
+                    belts.innerHTML = `Green Belt Level`;
+                    break;
+                //If points range 51-100
+                case points >= 50:
+                    console.log("Orange")
+                    belts.innerHTML = `Orange Belt Level`;
+                    break;
+                //If points range 0-50
+                default:                   
+                    console.log("White")
+                    belts.innerHTML = `White Belt Level`;         
+                }
+    // })
 }
     // apiActions.getRequest("https://localhost:44306/api/activities/points", points =>{
     //             belts.innerHTML = Point(points);
