848f862c8c4826890b9bd8e785223e3ae6e6668b
Bryan Kautzman <bdkautz@icloud.com>
2019-12-02 10:56:36 -0500

    front and backend imnages

--- Backend/FocusBackend/Migrations/20191127165759_addimages.Designer.cs
+++ Backend/FocusBackend/Migrations/20191127165759_addimages.Designer.cs
@@ -4,14 +4,16 @@ using FocusBackend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FocusBackend.Migrations
 {
     [DbContext(typeof(FocusContext))]
-    partial class FocusContextModelSnapshot : ModelSnapshot
+    [Migration("20191127165759_addimages")]
+    partial class addimages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -50,9 +52,9 @@ namespace FocusBackend.Migrations
                     b.ToTable("Activities");
 
                     b.HasData(
-                        new { ID = 1, CategoryID = 2, Completion = new DateTime(2019, 11, 27, 12, 9, 58, 702, DateTimeKind.Local), Creation = new DateTime(2019, 11, 27, 12, 9, 58, 700, DateTimeKind.Local), Description = "Outsourced From Temp Agency", Done = false, Importance = 9, Name = "Pay Workers", Rank = 18, Urgency = 9 },
-                        new { ID = 2, CategoryID = 6, Completion = new DateTime(2019, 11, 27, 12, 9, 58, 702, DateTimeKind.Local), Creation = new DateTime(2019, 11, 27, 12, 9, 58, 702, DateTimeKind.Local), Description = "Follow Up With Prospects", Done = false, Importance = 10, Name = "Sales Planning", Rank = 13, Urgency = 3 },
-                        new { ID = 3, CategoryID = 8, Completion = new DateTime(2019, 11, 27, 12, 9, 58, 702, DateTimeKind.Local), Creation = new DateTime(2019, 11, 27, 12, 9, 58, 702, DateTimeKind.Local), Description = "Get More Stamps For Pay Checks", Done = false, Importance = 4, Name = "Buy Stamps", Rank = 10, Urgency = 6 }
+                        new { ID = 1, CategoryID = 2, Completion = new DateTime(2019, 11, 27, 11, 57, 58, 632, DateTimeKind.Local), Creation = new DateTime(2019, 11, 27, 11, 57, 58, 630, DateTimeKind.Local), Description = "Outsourced From Temp Agency", Done = false, Importance = 9, Name = "Pay Workers", Rank = 18, Urgency = 9 },
+                        new { ID = 2, CategoryID = 6, Completion = new DateTime(2019, 11, 27, 11, 57, 58, 633, DateTimeKind.Local), Creation = new DateTime(2019, 11, 27, 11, 57, 58, 633, DateTimeKind.Local), Description = "Follow Up With Prospects", Done = false, Importance = 10, Name = "Sales Planning", Rank = 13, Urgency = 3 },
+                        new { ID = 3, CategoryID = 8, Completion = new DateTime(2019, 11, 27, 11, 57, 58, 633, DateTimeKind.Local), Creation = new DateTime(2019, 11, 27, 11, 57, 58, 633, DateTimeKind.Local), Description = "Get More Stamps For Pay Checks", Done = false, Importance = 4, Name = "Buy Stamps", Rank = 10, Urgency = 6 }
                     );
                 });
 
@@ -77,7 +79,7 @@ namespace FocusBackend.Migrations
                     b.ToTable("Businesses");
 
                     b.HasData(
-                        new { ID = 1, Image = "image", Industry = "Restaurant", Name = "Bertha and Bubba's Seafood Palace", UserID = 1 }
+                        new { ID = 1, Image = "Image", Industry = "Restaurant", Name = "Bertha and Bubba's Seafood Palace", UserID = 1 }
                     );
                 });
 
