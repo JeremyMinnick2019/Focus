2ee0a57e6ce561ced465e0b80c31d6ce09898217
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-12-02 10:32:51 -0500

    update to display

--- Frontend/src/js/components/home.js
+++ Frontend/src/js/components/home.js
@@ -8,7 +8,9 @@ export default () => {
 function displayHome(){
     const app = document.querySelector("#app");
     const sign = document.querySelector("#sign")
+    const belts = document.querySelector("#belt");
     sign.innerHTML = ``
+    belts.innerHTML = ``;
     apiActions.getRequest(`https://localhost:44306/api/activities/suggest`, activities =>{
     app.innerHTML = ActivityRank(activities);
     });
