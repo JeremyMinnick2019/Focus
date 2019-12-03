d8da33f236997282256e246b99f7fd8bddf26878
Jeremy <JeremyMinnick2019@Gmail.com>
2019-12-03 12:46:44 -0500

    finished progress bar

--- Frontend/src/js/components/progressbar.js
+++ Frontend/src/js/components/progressbar.js
@@ -11,36 +11,27 @@ function Bar() {
         switch (true){
             //If points range 501-1000
             case points >= 500:
-                bars.innerHTML = `<section class="userGrid">
-                <section class="user" id="gridItemA">
-                <img src="/images/black_belt.png" class=beltIMG></img>
-                <h3>Level: Black Belt</h3>
-                </section>
-                </section>`;
+                bars.innerHTML = `<div class="progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+              </div>`;
                 break;
             //If points range 251-500
             case points >= 250:
-                bars.innerHTML = `<section class="userGrid">
-                <section class="user" id="gridItemA">
-                <img src="/images/brown_belt.png" class=beltIMG></img>
-                <h3>Level: Brown Belt</h3>
-                </section>
-                </section>`;
+                bars.innerHTML = `<div class="progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 80%"></div>
+              </div>`;
                 break;
             //If points range 101-250
             case points >= 100:
-                bars.innerHTML = `<section class="userGrid">
-                <section class="user" id="gridItemA">
-                <img src="/images/green_belt.png" class=beltIMG></img>
-                <h3>Level: Green Belt</h3>
-                </section>
-                </section>`;
+                bars.innerHTML = `<div class="progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 60%"></div>
+              </div>`;
                 break;
             //If points range 51-100
             case points >= 50:
-                bars.innerHTML = `<section><div class="progress">
+                bars.innerHTML = `<div class="progress">
                 <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 40%"></div>
-              </div></section>`;
+              </div>`;
                 break;
             //If points range 0-50
             case points >= 0:
