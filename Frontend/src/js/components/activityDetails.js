adce900aa8b29d9d0daa29fba7b1126a4bcfa36f
Andy Kohler <andykohler99@gmail.com>
2019-12-03 11:41:30 -0500

    Fixed migration data, removed Points and hard coded point values

--- Frontend/src/js/components/activityDetails.js
+++ Frontend/src/js/components/activityDetails.js
@@ -4,13 +4,14 @@ export default function ActivityDetails(detailActivity) {
     var timeCreated = moment(detailActivity.creation).format('MMMM Do YYYY, h:mm a');
     
     return `
-        <section>
-            <h3>${detailActivity.name}</h3>
-            <p>${detailActivity.description}</p>
-            <h3>Importance: ${detailActivity.importance}</h3>
-            <h3>Urgency: ${detailActivity.urgency}</h3>
-            <h3> Time Created: ${timeCreated}</h3>
-            
+        <section class="userGrid">
+            <section class="user" id="gridItemA">
+                <h3 class="userName">${detailActivity.name}</h3>
+                <h3 id="cathead">${detailActivity.description}</p>
+                <h3>Importance: ${detailActivity.importance}</h3>
+                <h3>Urgency: ${detailActivity.urgency}</h3>
+                <h3 id="cathead"> Time Created: ${timeCreated}</h3>
+            </section>
             
         </section>
             `
