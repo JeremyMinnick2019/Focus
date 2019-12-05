cdd7e30898ee500ae84aa51b3b5db8374cb8d558
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-12-05 10:23:37 -0500

    typos

--- Frontend/src/js/components/success.js
+++ Frontend/src/js/components/success.js
@@ -13,7 +13,7 @@ export default function Success(activities) {
                 <section id="Success">
                 <section class="Success">
                     <h3 class="activity_name">${activity.name}</h3>
-                    <p class="activity_point">Point Value: ${activity.rank}
+                    <p class="activity_point">Points Earned: ${activity.rank}
                     <p class="activity_description">${activity.description}</p>
                     <p class="activity_creation">Started: <br>${timeCreated}</p>
                     <p class="activity_completion">Completed: <br>${timeCompleted}</p>
